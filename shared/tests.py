a9859195898f5989e425df63c2c9bdb4cf6bb3ce
FriedrichK <friedrich@cartogami.com>
2014-08-17 15:06:45 +0200

    improved readibility

--- shared/tests.py
+++ shared/tests.py
@@ -10,14 +10,10 @@ class ConversionTestCase(TestCase):
 
     def test_returns_expected_json_representation_of_datetime(self):
         dt = datetime(2014, 1, 1).replace(tzinfo=utc)
-
         actual = datetime_to_json(dt)
-
         self.assertEqual(actual, {'hour': 0, 'month': 1, 'second': 0, 'year': 2014, 'timezone': 'UTC', 'day': 1, 'minute': 0})
 
     def test_returns_expected_json_representation_of_datetime_without_timezone_information(self):
         dt = datetime(2014, 1, 1)
-
         actual = datetime_to_json(dt)
-
         self.assertEqual(actual, {'hour': 0, 'month': 1, 'second': 0, 'year': 2014, 'timezone': None, 'day': 1, 'minute': 0})
